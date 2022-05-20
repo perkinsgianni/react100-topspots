49c5d6323099fe33ad055b0ae275d0c46480fcbb
Gianni P <perkinsgianni@gmail.com>
2022-05-20 11:10:22 -0400

    Add assets folder with project photos, update README.md, modify topspot.jsx

---
+++ README.md
@@ -0,0 +1,5 @@
+## This app is deployed on Heroku: 
+## https://gp-top-spots.herokuapp.com
+
+![Project photo 1](assets/top_spots.png)
+![Project photo 2](assets/top_spots2.png)
--- src/topspot.jsx
+++ src/topspot.jsx
@@ -2,13 +2,11 @@ import React from 'react';
 
 // create presentational, stateless component
 export default props => (
-    <div style={ { overflow: 'scroll' } }>
     <div className='well'>
         <h4>{props.name}</h4>
         <p>{props.description}</p>
         <a className='btn btn-primary' href={ `https://maps.google.com/?q=${props.location[0]},${props.location[1]}` }>
             Open in Google Maps
         </a>
-    </div>
     </div>
 );
